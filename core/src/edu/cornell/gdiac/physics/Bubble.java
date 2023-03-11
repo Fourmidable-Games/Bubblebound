c06eb565f5b7461b002246ed8be5f3e179a25e73
Diarado <jt828@cornell.edu>
2023-03-11 00:30:44 -0500

    Added a bubble class so that we can add more features to bubbles  -- Johnson

---
+++ core/src/edu/cornell/gdiac/physics/Bubble.java
@@ -0,0 +1,12 @@
+package edu.cornell.gdiac.physics;
+import com.badlogic.gdx.math.*;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.physics.box2d.*;
+
+import com.badlogic.gdx.utils.JsonValue;
+import edu.cornell.gdiac.physics.*;
+import edu.cornell.gdiac.physics.obstacle.*;
+
+public class Bubble {
+
+}
