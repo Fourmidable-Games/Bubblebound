722481ab4d43f198b03a7f6aed9cb55790edfe66
qWorld-Machine <troygonzalez123@gmail.com>
2023-05-19 18:26:43 -0400

    fixed last commit

--- core/src/edu/cornell/gdiac/bubblebound/LevelEditorV2.java
+++ core/src/edu/cornell/gdiac/bubblebound/LevelEditorV2.java
@@ -536,7 +536,7 @@ public class LevelEditorV2 {
                                     arrayOffset = 332-1;
                                 }
 
-                                else if (k > 802) {
+                                else if (k > 802 || k<0) {
                                     flip = true;
                                 }
 
